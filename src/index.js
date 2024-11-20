f88999f5db77f52b0375614083a044e522ef922a
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-11-20 22:30:41 +0000

    use props

--- src/index.js
+++ src/index.js
@@ -61,30 +61,27 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza
-        name='Pizza Spinachi'
-        ingredients='Tomato, mozerella, spinach and ricotta cheese'
-        photoName='/pizzas/spinaci.jpg'
-        price={10}
-      />
-
-      <Pizza
-        name='Pizza Spinachi'
-        ingredients='Tomato, mozerella, spinach and ricotta cheese'
-        photoName='/pizzas/spinaci.jpg'
-        price={20}
-      />
+      {pizzaData.map((pizza) => (
+        <Pizza
+          name={pizza.name}
+          ingredients={pizza.ingredients}
+          photoName={pizza.photoName}
+          price={pizza.price}
+        />
+      ))}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div>
+    <div className='pizza'>
       <img src={props.photoName} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
-      <p>{props.price}</p>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <p>{props.price}</p>
+      </div>
     </div>
   );
 }
